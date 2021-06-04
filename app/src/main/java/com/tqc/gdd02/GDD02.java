0c91300a8399bb0b7b84b2dbe4bae82cd791db78
TomLin <tomwebdisk03@gmail.com>
2021-06-04 12:15:48 +0800

    1A2B

--- app/src/main/java/com/tqc/gdd02/GDD02.java
+++ app/src/main/java/com/tqc/gdd02/GDD02.java
@@ -2,6 +2,7 @@ package com.tqc.gdd02;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -37,21 +38,52 @@ public class GDD02 extends Activity implements View.OnClickListener
     int num=0;
     String strNum = "";
     do{
-      num = (int) (Math.random()*1000);
-      strNum = String.format("%03d",num);
+      num = (int) (Math.random()*1000);//12 5
+      strNum = String.format("%03d",num);//012 005
     }while(isLegal(strNum)==false);
+
+    Log.i("ANS",strNum);
     return strNum;
   }
 
   //判斷輸入數字符合規則
   private boolean isLegal(String num){
-      // TO DO
-
-
-  return true;
+    //TODO 記得修改
+    //檢查長度
+    if(num==null||num.length()!=3){
+      return false;
+    }
+    //123 1
+    if(num.charAt(0)==num.charAt(1)
+      ||num.charAt(0)==num.charAt(2) ||num.charAt(1)==num.charAt(2)
+    ){
+      return false;
+    }
+    return true;
   }
 // TO DO
+  private int getANum(String num,String luckNum){
+    int aNum =0;
+    for (int i = 0; i <luckyNum.length() ; i++) {
+      if(num.charAt(i)==luckyNum.charAt(i)){
+        aNum++;
+      }
+    }
+    return aNum;
+  }
+  private int getBNum(String num,String luckNum){
+    int bNum =0;
+    for (int i = 0; i <luckyNum.length() ; i++) {
+      char x = luckyNum.charAt(i);
+      if(num.charAt(i)==luckyNum.charAt(i)){
 
+      }else{
+        int pos = num.indexOf(x);
+        if(pos>=0)bNum++;
+      }
+    }
+    return bNum;
+  }
 
 
   //按鈕事件
@@ -59,6 +91,25 @@ public class GDD02 extends Activity implements View.OnClickListener
   public void onClick(View v) {
     String userNum = edInputNum.getText().toString();
     // TO DO
+    if(!isLegal(userNum)){
+      edInputNum.setText("");
+      String msg = "輸入錯誤"+getText(R.string.title);
+      tvRespond.setText(msg);
+    }else{
+      int numA = getANum(userNum,luckyNum);
+      int numB = getBNum(userNum,luckyNum);
+      //
+      if(numA==luckyNum.length()){
+        String msg = String.format(
+                "INPUT的數字: %s 恭喜你答對了!答案 : %s",userNum,luckyNum);
+        tvRespond.setText(msg);
+      }else{
+        edInputNum.setText("");
+        String msg = String.format(
+                "INPUT的數字: %s %dA%dB",userNum,numA,numB);
+        tvRespond.setText(msg);
+      }
+    }
 
     }
   }
